61a3a7ed991999e7e43bb811e4a21ee13febc10f
noeulgim <113411111+noeulgim@users.noreply.github.com>
2024-06-19 19:46:32 +0900

    Update common.js

--- numberBaseball(노을)/common.js
+++ numberBaseball(노을)/common.js
@@ -13,10 +13,16 @@ randomNum = [...new Set(String(randomNum).replace('.',''))].join('');
 //최종 랜덤숫자
 let getRandomNum = randomNum.substring(0,3);
 
+//prompt로 숫자 입력
 let input = String(prompt('컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!'));
+//시도 횟수 변수
 let countTry = 0;
+//count S 변수
 let countS = 0;
+//count B 변수
 let countB = 0;
+
+//일치할때까지 반복하는 반복문
 while(true){
 	countTry++;
 	for(var i = 0; i < getRandomNum.length; i++){
